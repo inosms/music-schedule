5d1f43a87f7de476c473cab2661331cd68e7dd13
inosms <4201657+inosms@users.noreply.github.com>
2024-01-04 21:29:38 +0900

    [refactor] use spotify library for auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,44 @@
-import './App.css';
-
-import { tryAuthorizeStep2TokenExchange } from './auth/auth';
-import { useTokenStorage } from './auth/tokenStorage';
-import LoginButton from './LoginButton';
 import { useEffect } from 'react';
+import './App.css';
+import { useSpotify } from './hooks/useSpotify';
+
+const clientId = '553fb95155e2496292584bc36823741d';
+const redirectUrlDev = 'http://localhost:3000';
+const redirectUrlProd = 'https://music-time-table.simonschlegl.com';
+const scopes = ['user-read-currently-playing', 'user-read-playback-state'];
+
+// Returns true if the current environment is a development environment
+function isDevelopmentEnvironment(): boolean {
+  // taken from https://stackoverflow.com/questions/35469836/detecting-production-vs-development-react-at-runtime
+  return !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
+}
+
+function getRedirectUrl(): string {
+  return isDevelopmentEnvironment() ? redirectUrlDev : redirectUrlProd;
+}
 
 function App() {
-  const { setToken, isLoggedIn } = useTokenStorage();
+  const sdk = useSpotify(
+    clientId,
+    getRedirectUrl(),
+    scopes,
+  );
 
   useEffect(() => {
-    tryAuthorizeStep2TokenExchange().then((token) => {
-      if (token) {
-        setToken(token);
+    async function test() {
+      if (!sdk) {
+        return;
       }
-    });
-  }, [setToken]);
+      console.log(await sdk?.player?.getCurrentlyPlayingTrack());
+    }
+
+    test();
+  }, [sdk]);
+
 
   return (
     <div className="App">
       <header className="App-header">
-        {isLoggedIn ? (
-          <p>
-            You are  logged in!
-          </p>
-        ) : (
-          <LoginButton />
-        )}
       </header>
     </div>
   );
--- src/LoginButton.tsx
+++
@@ -1,9 +0,0 @@
-import { authorizeStep1UserAuth } from './auth/auth';
-
-function LoginButton() {
-    return (
-        <button onClick={() => authorizeStep1UserAuth()}>Log In</button>
-    );
-}
-
-export default LoginButton;
