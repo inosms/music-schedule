df6c4033694983cf99f4c20de64a543e7da83500
inosms <4201657+inosms@users.noreply.github.com>
2024-01-05 20:29:14 +0900

    [add] router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import { useEffect, useState } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import { useSpotify } from './hooks/useSpotify';
-import { SimplifiedPlaylist, SpotifyApi } from '@spotify/web-api-ts-sdk';
-import { TimeTableWithPlaylist, parseTimeTable } from './timetable';
-import PlaylistElement from './PlaylistElement';
+import UserPlaylistSelection from './UserPlaylistSelection';
 
 const clientId = '553fb95155e2496292584bc36823741d';
 const redirectUrlDev = 'http://localhost:3000';
@@ -20,39 +18,6 @@ function getRedirectUrl(): string {
   return isDevelopmentEnvironment() ? redirectUrlDev : redirectUrlProd;
 }
 
-// Returns all playlists for the current user
-async function getAllPlaylistsForCurrentUser(sdk: SpotifyApi): Promise<SimplifiedPlaylist[]> {
-  let playlists: SimplifiedPlaylist[] = [];
-
-  while (true) {
-    const response = await sdk.currentUser.playlists.playlists(50, playlists.length);
-    playlists = playlists.concat(response.items);
-
-    if (!response.next) {
-      break;
-    }
-  }
-
-  return playlists;
-}
-
-// Returns all playlists for the current user that have a timetable in their description
-async function getAllPlaylistsWithTimeTable(sdk: SpotifyApi): Promise<TimeTableWithPlaylist[]> {
-  const playlists = await getAllPlaylistsForCurrentUser(sdk);
-
-  // Of all the playlists the user has, only keep the ones that have a timetable in their description
-  const timeTableWithPlaylists = playlists.flatMap((playlist) => {
-    const timeTable = parseTimeTable(playlist.description);
-
-    if (timeTable) {
-      return [{ timeTable, playlist } as unknown as TimeTableWithPlaylist];
-    }
-    return [];
-  });
-
-  return timeTableWithPlaylists;
-}
-
 function App() {
   const sdk = useSpotify(
     clientId,
@@ -60,30 +25,12 @@ function App() {
     scopes,
   );
 
-  const [timeTableWithPlaylistState, setTimeTableWithPlaylistState] = useState<TimeTableWithPlaylist[]>([]);
-
-  useEffect(() => {
-    (async () => {
-      if (sdk) {
-        const playlists = await getAllPlaylistsWithTimeTable(sdk);
-        setTimeTableWithPlaylistState(playlists);
-      }
-    })();
-  }, [sdk]);
-
-
   return (
-    <div className="App">
-      <div>
-        {
-          timeTableWithPlaylistState.map((timeTableWithPlaylist) => {
-            return (
-              <PlaylistElement key={timeTableWithPlaylist.playlist.id} timeTableWithPlaylist={timeTableWithPlaylist} />
-            );
-          })
-        }
-      </div>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <UserPlaylistSelection sdk={sdk} />
+      </div >
+    </BrowserRouter>
   );
 }
 
--- src/PlaylistElement.tsx
+++ src/PlaylistElement.tsx
@@ -1,9 +1,16 @@
+import { Link } from 'react-router-dom';
 import { TimeTableWithPlaylist } from "./timetable";
 
 export default function PlaylistElement({ timeTableWithPlaylist }: { timeTableWithPlaylist: TimeTableWithPlaylist }) {
     return (
         <div>
-            {timeTableWithPlaylist.playlist.name} by {timeTableWithPlaylist.playlist.owner.display_name}
+            <Link
+                to={{
+                    search: '?playlist=' + timeTableWithPlaylist.playlist.id,
+                }}
+            >
+                {timeTableWithPlaylist.playlist.name} by {timeTableWithPlaylist.playlist.owner.display_name}
+            </Link>
         </div>
     );
 }
