abb3e317487640c642dd16a2a5d1e75cd3058b3c
inosms <4201657+inosms@users.noreply.github.com>
2024-01-06 17:31:12 +0900

    [refactor] ScheduleSlot

--- src/components/ScheduleSlot.tsx
+++ src/components/ScheduleSlot.tsx
@@ -2,66 +2,53 @@ import { SpotifyApi } from "@spotify/web-api-ts-sdk";
 import { SlotWithTracks } from "../schedule";
 import { useEffect } from "react";
 
-/*
-add a "syncing" state.
-if in syncing state:
-    if block is active: check every n seconds: is the currently playing track in the current block?
-        if yes: check the queue: is the next track in the current block?
-            if yes: do nothing
-            if no: queue the next track in the current block
-        if no: check whether the current song will end within x seconds
-            if yes: queue the next song from this block
-            if no: force skip play a song from this block and queue the next song from this block
-
-
-n = 10 seconds
-x = 120 seconds
-*/
-
+// Skip over the next N tracks in the queue
 async function skipNTimes(spotify: SpotifyApi, elementsToSkip: number) {
     for (let i = 0; i < elementsToSkip; i++) {
         await spotify.player.skipToNext(""); // empty device id means current device
     }
 }
 
+// Skip until the next track in the queue is in the slot
 async function skipUntilTrackInSlot(spotify: SpotifyApi, slot: SlotWithTracks) {
     const queue = await spotify.player.getUsersQueue();
-
     const indexOfQueuedTrackInSlot = queue.queue.findIndex((track) => slot.containsTrack(track.id));
-    const queueHasTrackInSlot = indexOfQueuedTrackInSlot !== -1;
 
-    if (queueHasTrackInSlot) {
+    if (indexOfQueuedTrackInSlot !== -1) {
         await skipNTimes(spotify, indexOfQueuedTrackInSlot + 1);
     } else {
         console.error("Queue does not contain track in slot");
     }
 }
 
+// Modify the currently playing track and the queue of the player so that it plays a track in the slot.
+// If it is already playing a track in the slot, it will do nothing.
 async function syncSlot(slot: SlotWithTracks, spotify: SpotifyApi) {
     const queue = await spotify.player.getUsersQueue();
 
     const currentTrack = queue.currently_playing?.id;
+    const currentlyPlayingTrackInSlot = currentTrack && slot.containsTrack(currentTrack);
 
-    const currentTrackInSlot = currentTrack && slot.containsTrack(currentTrack);
     const indexOfQueuedTrackInSlot = queue.queue.findIndex((track) => slot.containsTrack(track.id));
     const queueHasTrackInSlot = indexOfQueuedTrackInSlot !== -1;
 
+    // Always make sure there is a track in the queue that is in the slot
     if (!queueHasTrackInSlot) {
-        const nextTrackInSlot = currentTrack && currentTrackInSlot ? slot.getTrackAfter(currentTrack) : slot.getTrackByIndex(0);
+        // We want to play tracks in order of the playlist
+        const nextTrackInSlot = currentTrack && currentlyPlayingTrackInSlot ? slot.getTrackAfter(currentTrack) : slot.getTrackByIndex(0);
         if (nextTrackInSlot) {
             await spotify.player.addItemToPlaybackQueue(nextTrackInSlot.track.uri, ""); // empty device id means current device
         }
     }
 
-    if (!currentTrackInSlot) {
-        const currentTrackLengthSec = queue.currently_playing?.duration_ms || 0 / 1000;
+    if (!currentlyPlayingTrackInSlot) {
+        const currentTrackLengthSec = (queue.currently_playing?.duration_ms || 0) / 1000;
 
         // For songs that are almost finished we will just let them finish
         if (currentTrackLengthSec < 180) {
             console.debug("Will let current track finish: ", queue.currently_playing?.name);
         }
         else {
-            console.debug("Will skip over queue: ", queue);
             await skipUntilTrackInSlot(spotify, slot);
         }
     }
