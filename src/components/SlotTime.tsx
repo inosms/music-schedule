9172313e32295e1ab42799dee33c98b7f45c3d90
inosms <4201657+inosms@users.noreply.github.com>
2024-01-07 13:53:31 +0900

    [add] remove button

--- src/components/SlotTime.tsx
+++ src/components/SlotTime.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import "./SlotTime.css";
 
 function minutesToTimeString(minutes: number) {
     const hours = Math.floor(minutes / 60);
@@ -17,9 +18,9 @@ function parseTimeString(timeString: string): number | null {
 export function SlotTime({ time, setTime, minTime, maxTime, onRemove }: { time: number, setTime: (time: number) => void, minTime: number, maxTime: number, onRemove?: () => void }) {
     const [timeState, setTimeState] = useState(minutesToTimeString(time));
     const [timeStateValid, setTimeStateValid] = useState(true);
-    
+
     return (
-        <div>
+        <div className="slot-time">
             <input
                 style={{ borderColor: timeStateValid ? "black" : "red" }}
                 type="text"
@@ -33,16 +34,20 @@ export function SlotTime({ time, setTime, minTime, maxTime, onRemove }: { time: 
                     } else {
                         setTimeStateValid(false);
                     }
-                }} 
+                }}
                 onBlur={() => {
                     const newTime = parseTimeString(timeState);
                     if (newTime && timeStateValid) {
                         setTime(newTime);
                     }
                 }}
-                />
+            />
 
-            {onRemove ? <button onClick={() => onRemove()}>Remove Slot</button> : null}
+            {onRemove ? <button className="removebutton" onClick={() => onRemove()}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-x" viewBox="0 0 16 16">
+                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                </svg>
+            </button> : null}
         </div>
     );
 }
