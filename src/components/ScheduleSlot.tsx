e2df10f454504d8616e07a80b79f97dc9703cead
inosms <4201657+inosms@users.noreply.github.com>
2024-01-07 11:42:02 +0900

    [add] force skip until current song

--- src/components/ScheduleSlot.tsx
+++ src/components/ScheduleSlot.tsx
@@ -44,19 +44,11 @@ async function syncSlot(slot: SlotWithTracks, spotify: SpotifyApi) {
     }
 
     if (!currentlyPlayingTrackInSlot) {
-        const currentTrackLengthSec = (queue.currently_playing?.duration_ms || 0) / 1000;
-
-        // For songs that are almost finished we will just let them finish
-        if (currentTrackLengthSec < 180) {
-            console.debug("Will let current track finish: ", queue.currently_playing?.name);
-        }
-        else {
-            await skipUntilTrackInSlot(spotify, slot);
-        }
+        await skipUntilTrackInSlot(spotify, slot);
     }
 }
 
-export default function ScheduleSlot({ spotify, slot, syncing, nextSlotLength, setLength, onRemoveTrack, onRemoveSlot }: { spotify: SpotifyApi | null, slot: SlotWithTracks, syncing: boolean, nextSlotLength: number, setLength: (time: number) => void, onRemoveTrack: (uri: string, id: string) => void , onRemoveSlot: () => void}) {
+export default function ScheduleSlot({ spotify, slot, syncing, nextSlotLength, setLength, onRemoveTrack, onRemoveSlot }: { spotify: SpotifyApi | null, slot: SlotWithTracks, syncing: boolean, nextSlotLength: number, setLength: (time: number) => void, onRemoveTrack: (uri: string, id: string) => void, onRemoveSlot: () => void }) {
     useEffect(() => {
         const checkIfPlaying = async () => {
             if (spotify && syncing && slot.shouldPlayNow() && !slot.isEmpty()) {
@@ -76,17 +68,17 @@ export default function ScheduleSlot({ spotify, slot, syncing, nextSlotLength, s
                     time={slot.getStartTimeMinutes()}
                     setTime={(_time) => console.debug("can not set time of first slot")}
                     minTime={0}
-                    maxTime={0} 
+                    maxTime={0}
                     onRemove={slot.isLastSlot() ? undefined : () => onRemoveSlot()}
-                    /> : null}
+                /> : null}
             <div>
                 {slot.getTracks().map((track, index) => {
                     return (
                         <SlotSongElement
                             key={`slot-song-${index}-${track.track.id}`}
-                            track={track} 
+                            track={track}
                             onRemove={() => onRemoveTrack(track.track.uri, track.track.id)}
-                            />
+                        />
                     );
                 })}
             </div>
@@ -95,9 +87,9 @@ export default function ScheduleSlot({ spotify, slot, syncing, nextSlotLength, s
                 time={slot.getStartTimeMinutes() + slot.getLengthMinutes()}
                 setTime={(time) => setLength(time - slot.getStartTimeMinutes())}
                 minTime={slot.getStartTimeMinutes() + 1}
-                maxTime={slot.getStartTimeMinutes() + slot.getLengthMinutes() + (Math.max(nextSlotLength - 1, 0))} 
+                maxTime={slot.getStartTimeMinutes() + slot.getLengthMinutes() + (Math.max(nextSlotLength - 1, 0))}
                 onRemove={slot.isLastSlot() ? undefined : () => onRemoveSlot()}
-                />
+            />
         </div>
     );
 }
